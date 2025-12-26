a5a7bd7622f45bf4ad69cf21a50ef886b68a7510
bruyeron <crandriafara@gmail.com>
2025-12-26 15:30:38 +0300

    Creation du bdd

--- skills/admin.py
+++ skills/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categorie, Profile, Competences
 
 # Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Profile)
+admin.site.register(Competences)
